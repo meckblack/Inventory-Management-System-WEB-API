ec4510522a28ff6fcc047f2e94451118130574be
meckblack <meckydrix@gmail.com>
2017-07-09 15:24:00 +0100

    Added Fixed Asset and Fixed AssetViewModel to the Client

---
+++ Client/ViewModels/FixedAssetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Client.Models;
+
+namespace Client.ViewModels
+{
+    public class FixedAssetViewModel
+    {
+        public FixedAsset fixedAsset
+        {
+            get;
+            set;
+        }
+    }
+}
