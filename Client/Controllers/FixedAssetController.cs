4eec8de7adcda2c2c7dd3c1950aa4bbad0182503
meckblack <meckydrix@gmail.com>
2017-07-09 15:38:56 +0100

    Added the Get function for index in the Client

---
+++ Client/Controllers/FixedAssetController.cs
@@ -0,0 +1,93 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class FixedAssetController : Controller
+    {
+        // GET: FixedAsset
+        public ActionResult Index()
+        {
+            FixedAssetClient fac = new FixedAssetClient();
+
+            ViewBag.FixedAssetList = fac.FindAll();
+            return View();
+        }
+
+        //// GET: FixedAsset/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        // GET: FixedAsset/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FixedAsset/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FixedAsset/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FixedAsset/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FixedAsset/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FixedAsset/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Client/Models/FixedAssetClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace Client.Models
+{
+    public class FixedAssetClient
+    {
+        private string BASE_URL = "http://localhost:64448/api/";
+
+        public IEnumerable<FixedAsset> FindAll()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("fixedAsset").Result;
+                if (response.IsSuccessStatusCode)
+                    return response.Content.ReadAsAsync<IEnumerable<FixedAsset>>().Result;
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public FixedAsset Find(int id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("fixedAsset/" + id).Result;
+                if (response.IsSuccessStatusCode)
+                    return response.Content.ReadAsAsync<FixedAsset>().Result;
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
