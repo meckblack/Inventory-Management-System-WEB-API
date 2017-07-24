c7845bdbd00f27f49048cd571b4f6ac5ff1b46e1
meckblack <meckydrix@gmail.com>
2017-07-25 00:10:27 +0100

    Added the Purchase Model to the API

---
+++ Inventory Management System/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory_Management_System.Models
+{
+    public class Purchase
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public Product ProductId  { get; set; }
+
+        public virtual Product PurchaseProduct { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
