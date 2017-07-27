bded6960d4d5fcf0cc2468e4b08dbb728c826d2d
meckblack <meckydrix@gmail.com>
2017-07-27 11:39:24 +0100

    Added the Purchase CLient

---
+++ Client/Models/PurchaseClient.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Client.Models
+{
+    public class PurchaseClient
+    {
+        private string BASE_URL = "http://localhost:64448/api/";
+
+        public IEnumerable<Purchase> FindAll()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("purchase").Result;
+                if (response.IsSuccessStatusCode)
+                    return response.Content.ReadAsAsync<IEnumerable<Purchase>>().Result;
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public Purchase Find(int id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("purchase/" + id).Result;
+                if (response.IsSuccessStatusCode)
+                    return response.Content.ReadAsAsync<Purchase>().Result;
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Create(Purchase purchase)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PostAsJsonAsync("purchase",
+                    purchase).Result;
+                return response.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.DeleteAsync("purchase/" +
+                    id).Result;
+                return response.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Edit(Purchase purchase)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PutAsJsonAsync("purchase/" + purchase.Id,
+                    purchase).Result;
+                return response.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Client/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Client.ViewModels
+{
+    public class PurchaseViewModel
+    {
+        public Purchase purchae
+        {
+            get;
+            set;
+        }
+    }
+}
